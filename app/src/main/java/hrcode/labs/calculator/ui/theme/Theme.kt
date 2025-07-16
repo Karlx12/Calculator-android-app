9b7071d46b418d8cf34c2891fcb269336fcb02c4
RONALD FABRIZIO HURTADO RAMOS <maxx1212_2@hotmail.com>
2025-07-16 18:15:46 -0500

    Remove unused color definitions and clean up theme imports

--- app/src/main/java/hrcode/labs/calculator/ui/theme/Theme.kt
+++ app/src/main/java/hrcode/labs/calculator/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package hrcode.labs.calculator.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
