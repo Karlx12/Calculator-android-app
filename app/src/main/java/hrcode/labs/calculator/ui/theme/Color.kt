7cfe3d796b8be4bfd2e50b12e0b7d309f789f1de
RONALD FABRIZIO HURTADO RAMOS <maxx1212_2@hotmail.com>
2025-07-15 22:20:39 -0500

    Initial commit

---
+++ app/src/main/java/hrcode/labs/calculator/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package hrcode.labs.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val ShakespeareLight01 = Color(0xFF6DC4E2)
+val Shakespeare = Color(0xFF46A1C4)
+val ShakespeareDark01 = Color(0xFF2984AB)
+val ShakespeareDark02 = Color(0xFF0B6690)
+val NileBlueLight02 = Color(0xFF2C7287)
+val NileBlueLight01 = Color(0xFF18546D)
+val NileBlue = Color(0xFF043C59)
+val NileBlueDark01 = Color(0xFF01304C)
